291a44bba1d07bc17ecb9bc156103008a06481f1
Sahil Satralkar <sahilsatralkar@Sahils-MacBook-Air.local>
2020-08-14 02:00:32 +0530

    Add Challenge 1 project with comments

--- UIKit/Challenge1/Challenge1/DetailViewController.swift
+++ UIKit/Challenge1/Challenge1/DetailViewController.swift
@@ -9,22 +9,57 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    //IBOutlet for Flag display image.
+    @IBOutlet var flagImage: UIImageView!
+    //Variable to store the selected file name
+    var selectedImage : String?
+    //Variable to store the flag name
+    var flagName = ""
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //If let condition to avoid null error.
+        if let imageToDisplay = selectedImage {
+            flagImage.image = UIImage(named: imageToDisplay)
+            //This logic to remove the @2x suffix from flag name.
+            for c in imageToDisplay {
+                if c == "@" {
+                    break
+                } else {
+                    flagName.append(c)
+                }
+            }
+        }
+        
+        // flag name is coverted to Upper case before assigning to navigation bar title text.
+        title = flagName.uppercased()
+        //Default implementation to keep the navigation bar text small on this screen.
+        navigationItem.largeTitleDisplayMode = .never
+        //Code will create a button on the right side of Navigation bar. This is to enable sharing of the image.
+        //New UIBarButtonItem instance is created. barButtonSystemItem is chosen as the in-built action button image. trget parameter is self because the
+        //function to be called is within self class. action parameter is given #selector which again contains the method to be called on button clicked.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareButtonTapped))
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //@objc annotation is mandatry because #selector is a objective C code.
+    @objc func shareButtonTapped() {
+        
+        //the image is downgraded to 80% quality to save space with the jpegData method.
+        guard let image = flagImage.image?.jpegData(compressionQuality: 0.8) else {
+            print("Image not found")
+            return
+        }
+        
+        //New UIActivityViewController object is created. The image is passed inside the activityItems array.
+        let avc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        //This implementation is necessary for iPad
+        avc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        //Present the UIActivityViewController object.
+        present(avc, animated: true)
+        
     }
-    */
-
+    
 }
--- UIKit/Challenge1/Challenge1/ViewController.swift
+++ UIKit/Challenge1/Challenge1/ViewController.swift
@@ -8,13 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    //array to store the names of flag.
+    var flagNameArray = [String]()
+    
+    //Code to remove the "@2x" suffix to file name within array.
+    var updateImageName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //default implementation for Filemanager to handle files in Swift
+        let fm = FileManager.default
+        //default path for files in project. Can be safely unwrapped.
+        let path = Bundle.main.resourcePath!
+        //This will create an array of NSString with the names of files
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        //Create a flagNameArray which will filter files containing 2x in name.
+        for item in items {
+            if item.contains("2x") {
+                flagNameArray.append(item)
+            }
+        }
+        //default implmentation for Navigation bar text.
+        title = "Challenge 1"
+        //Apple recommended styling for Navigation bar.
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
+    
+    //Mandatory implementation of this method override.
+    //Need to return the number of rows in teh UITableView.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flagNameArray.count
+    }
+    
+    //Mandatory implementation of this method override.
+    //This method is called internally for every time for a row in Table View.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Creating a new reusable cell with Identifier for cell as given in main.storyboat attributes inspector.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FlagCell", for: indexPath)
+        let imageInACell = UIImage(named: flagNameArray[indexPath.row])
+        
+        //Code to remove the "@2x" suffix to file name within array.
+        var updateImageName = ""
+        for c in flagNameArray[indexPath.row] {
+            if c == "@" {
+                break
+            } else {
+                updateImageName.append(c)
+            }
+        }
+        
+        //make all strings within the array to uppercase.
+        updateImageName = updateImageName.uppercased()
+        //set image
+        cell.imageView?.image = imageInACell
+        //set text
+        cell.textLabel?.text = updateImageName
+        return cell
+    }
+    
+    //This method will detect the tap on the cell by user and redirect to next screen.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //Create an instance of DetailViewController from the storyboard.
+        if let dvc = storyboard?.instantiateViewController(identifier: "SecondScreen") as? DetailViewController {
+            //set image in DetailViewController as per selected row.
+            dvc.selectedImage = flagNameArray[indexPath.row]
+            //push the DetailViewController instance into the navigationController instance.
+            navigationController?.pushViewController(dvc, animated: true)
+        }
+        //Condition if no DetailViewController in main.storyboard.
+        else {
+            print("Detail view controller not available")
+        }
+    }
 }
 
