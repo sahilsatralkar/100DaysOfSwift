2c10d2df2a836ab939da409c56a1da50116cd90e
Sahil Satralkar <sahilsatralkar@Sahils-MacBook-Air.local>
2020-08-14 00:17:37 +0530

    Project 3 code comments added

--- UIKit/Project3/Project 3/SecondViewController.swift
+++ UIKit/Project3/Project 3/SecondViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
     
+    //IBoutlet for UIImageView to display photo.
     @IBOutlet var stormImage: UIImageView!
     var selectedImage: String?
     var fileNameArrayLabelSVC = [String]()
@@ -18,21 +18,28 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //if let condition to avoid null error.
         if let imageToLoad = selectedImage {
+            //Set the image as per the selected cell.
             stormImage.image  = UIImage(named: imageToLoad)
+            //title is the default implementation for Navigation bar title text.
             title = "Image \(position + 1) of \(fileNameArrayLabelSVC.count)"
         }
+        
+        //this code is necessay to get the small font styling for navigation bar title text.
         navigationItem.largeTitleDisplayMode = .never
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.hidesBarsOnTap = false
-    }
-    
+    //this method is for the navigation bar at the top to disappear on users tap on photo.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.hidesBarsOnTap = true
     }
-
+    
+    //this method is to keep the previous screen behavior regarding navigation bar as was earlier.
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
 }
--- UIKit/Project3/Project 3/ViewController.swift
+++ UIKit/Project3/Project 3/ViewController.swift
@@ -13,46 +13,61 @@ class ViewController: UITableViewController {
     var fileNameArray = [String]()
     var fileNameArrayLabel = [String]()
     var position = Int()
+    var photosCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //title keyword is for the navigation bar title label text.
         title = "Storm viewer"
+        
+        //Default implementation to fetch the FileManager object for photo files handling
         let fm = FileManager.default
+        //Default path for the project directory. Can be safely force unwrapped.
         let path = Bundle.main.resourcePath!
+        //Will return the NSString array containg file names.
         let items = try! fm.contentsOfDirectory(atPath: path)
-        print(items)
-        var count = 0
+        
         for item in items {
+            //Our library of photos have prefix of nssl.
             if item.hasPrefix("nssl") {
-                print("Item : \(item)")
                 fileNameArray.append(item)
-                count += 1
-                fileNameArrayLabel.append("Image \(count)")
+                //variable to count the number of photos.
+                photosCount += 1
+                //This array will contain fabricated string to display on table cell
+                fileNameArrayLabel.append("Image \(photosCount)")
             }
         }
+        
+        //sort method on NSString array will sort the names of files alphabetically.
         fileNameArray.sort()
-        print("fileNameArray: \(fileNameArray)")
+        //Apple recommended styling of Navigation bar text.
         navigationController?.navigationBar.prefersLargeTitles = true
     }
 
+    //This method is compulsory implementation for UITableViewController
+    //Inside this method we need to return the number of rows in the table.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return fileNameArray.count
     }
     
+    //This method is compulsory implementation for UITableViewController
+    //We create a reusable cell and link it to datasource. Method is called for every cell in table.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Cell identifier is as given in Identifier in Attributes inspector for cell in main.storyboard
         let cell = tableView.dequeueReusableCell(withIdentifier: "FileNameRow", for: indexPath)
         cell.textLabel?.text = fileNameArrayLabel[indexPath.row]
         return cell
     }
     
+    //This method is called each time user taps on a cell in table.
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        //This will create an instance of SecondViewController from the storyboard
         if let vc = storyboard?.instantiateViewController(withIdentifier: "SecondScreen") as? SecondViewController {
-            // 2: success! Set its selectedImage property
+            // Set the values inside the properties with instance of SecondViewController i.e. vc
             vc.selectedImage = fileNameArray[indexPath.row]
             vc.fileNameArrayLabelSVC = fileNameArrayLabel
             vc.position = indexPath.row
-            // 3: now push it onto the navigation controller
+            //this will push the SecondViewController onto the navigationController object
             navigationController?.pushViewController(vc, animated: true)
         }
         
