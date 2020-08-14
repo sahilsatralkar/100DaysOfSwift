457d3f2271d20ba06c308a6dc36c92e107b094aa
Sahil Satralkar <sahilsatralkar@Sahils-MacBook-Air.local>
2020-08-14 13:53:17 +0530

    update SwiftUI directory

--- SwiftUI/Convert It/Convert It/ContentView.swift
+++ SwiftUI/Convert It/Convert It/ContentView.swift
@@ -9,9 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //Variable declared with @State annotation for 2 way binding.
     @State var userValueInKilograms = ""
     
+    //another varable declared to compute the result.
     var computedValueInPounds : Double {
+        //Typecasting userValueInKilograms to Double and also nill coalescence so if user inputs string it wont crash the app.
         let tempUserValueInKilograms = Double(userValueInKilograms) ?? 0
         let tempComputedValueInPounds = (tempUserValueInKilograms * 2.20462262185)
         return tempComputedValueInPounds
@@ -21,13 +25,17 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section(header: Text("Weight in Kilograms")) {
+                    //variable is marked with $ prefix so there is 2 way binding. Any update to this value will refresh the whole body every time.
                     TextField("Weight", text: $userValueInKilograms)
+                        //User will be shown keyboard of decimalPad type.
                         .keyboardType(.decimalPad)
                 }
                 Section(header: Text("Result")) {
+                    //the specifier will limt the result to 3 decimal places.
                     Text("\(computedValueInPounds, specifier: "%.3f") pounds")
                 }
             }
+            //this will display text on the Navigation bar.
             .navigationBarTitle("Convert It", displayMode: .large)
         }
         
--- SwiftUI/WeSplit/WeSplit/ContentView.swift
+++ SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -10,13 +10,22 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //The 3 required variables are declared with @State annotation so that it can have 2 way binding.
     @State private var checkAmount = ""
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
+    
+    //Array to show the possible tip percent.
     let tipPercentages = ["10", "15", "20", "25", "0"]
+    
+    //This variable will compute its value based on values other variables.
     var totalPerPerson : Double {
+        
+        //typecasting int to double
         let peopleCount = Double(numberOfPeople + 2)
+        //typecating tipPercentages to double with nill coalescence.
         let tipSelection = Double(tipPercentages[tipPercentage]) ?? 0
+        //typecasting checkamount string to double with nill coalescence.
         let orderAmount = Double(checkAmount) ?? 0
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
@@ -25,6 +34,8 @@ struct ContentView: View {
         return amountPerPerson
         
     }
+    
+    //Variable to compute total bill amount based on values of variables.
     var originalAmount : Double {
         let orderAmount = Double(checkAmount) ?? 0
         let tipSelection = Double(tipPercentages[tipPercentage]) ?? 0
@@ -36,35 +47,43 @@ struct ContentView: View {
         NavigationView {
             Form {
                 Section {
+                    //$ before checkamount means 2 way binding. Any change to checkAmount will refresh the whole body.
                     TextField("Amount", text: $checkAmount)
+                        //When user clicks on this field, keybad of decimal type is shown.
                         .keyboardType(.decimalPad)
+                    //this picker whill give optin to choose number of people from 2 to 100.
                     Picker ("Number of people" , selection: $numberOfPeople) {
                         ForEach ( 2 ..< 100) {
+                            //below code uses short hand for closure and $0 is the first parameter.
                             Text("\($0) people")
                         }
                     }
-                    
                 }
+                //Header within section means the text is displayed as one with Picker.
                 Section(header: Text("How much tip do you want to leave?")) {
+                    //2 way binding on this picker with tipPercentage. Any change to tipPercent will refresh the whole body.
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0 ..< tipPercentages.count) {
                             Text("\(self.tipPercentages[$0])%")
                         }
-                    
                     }
+                    //below code will display picke in segmented style.
                     .pickerStyle(SegmentedPickerStyle())
                 }
+                
                 Section(header: Text("Total Amount")) {
+                    //specifier will limit the double value to 2 decimal places
                     Text("$\(originalAmount, specifier: "%.2f")")
                 }
                 Section(header: Text("Amount per person")) {
+                    //specifier will limit the double value to 2 decimal places
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
             }
+            //code to display the navigation bar text
             .navigationBarTitle("WeSplit", displayMode: .inline)
         }
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
