8587d54204b825f009f5370f74e729ab74594771
Sahil Satralkar <sahilsatralkar@Sahils-MacBook-Air.local>
2020-08-22 20:40:06 +0530

    Add Project6a and 6b

---
+++ UIKit/Project6b/Project6b/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Sahil Satralkar on 22/08/20.
+//  Copyright © 2020 Sahil Satralkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Define label1 with some properties
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        //Define label2 with some properties
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        //Define label3 with some properties
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        //Define label4 with some properties
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        //Define label5 with some properties
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        //Add the label views to viwe of ViewController
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+
+        var previous: UILabel?
+
+        //This loop through all labels to set its constraints.
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: 0).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+
+            //This code is to provide distance between label views
+            if let previous = previous {
+                // we have a previous label – create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                // this is the first label
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+
+            // set the previous label to be the current one, for the next loop iteration
+            previous = label
+        }
+
+    }
+}
+
