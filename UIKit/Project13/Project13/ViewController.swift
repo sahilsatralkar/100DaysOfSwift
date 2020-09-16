ea5251050d1fadbe265219d0d9c6ba74fd98ba54
Sahil Satralkar <sahilsatralkar@Sahils-MacBook-Air.local>
2020-09-17 01:38:13 +0530

    add Proj13

--- UIKit/Project13/Project13/ViewController.swift
+++ UIKit/Project13/Project13/ViewController.swift
@@ -5,16 +5,146 @@
 //  Created by Sahil Satralkar on 16/09/20.
 //  Copyright © 2020 Sahil Satralkar. All rights reserved.
 //
-
+import CoreImage
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    @IBOutlet var changeFilterLabel: UIButton!
+    @IBOutlet var radius: UISlider!
+    @IBOutlet var scale: UISlider!
+    @IBOutlet var center: UISlider!
+    
+    var currentImage : UIImage!
+    var context : CIContext!
+    var currentFilter : CIFilter!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "InstaFilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+        changeFilterLabel.setTitle("CISepiaTone", for: .normal)
     }
-
-
+    
+    @objc func importPicture(){
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing(tag: nil)
+        
+        
+    }
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+        
+    }
+    
+    @objc func setFilter(action : UIAlertAction){
+        // make sure we have a valid image before continuing!
+        changeFilterLabel.setTitle(action.title, for: .normal)
+        
+        guard currentImage != nil else { return }
+        
+        // safely read the alert action's title
+        guard let actionTitle = action.title else { return }
+        currentFilter = CIFilter(name: actionTitle)
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing(tag: nil)
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        if let image = imageView.image {
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+            
+        else {
+            
+            let ac = UIAlertController(title: "Save error", message: "No image selected to save", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
+    }
+    
+    @IBAction func intensityChanged(_ sender: UISlider) {
+        
+        applyProcessing(tag : sender.tag)
+    }
+    
+    func applyProcessing(tag : Int?){
+        
+        let inputKeys = currentFilter.inputKeys
+        
+        switch tag {
+        case 0:
+            //
+            if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey) }
+        case 1:
+            //
+            if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(scale.value * 10, forKey: kCIInputScaleKey) }
+        case 2:
+            //
+            if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
+        case 3:
+            //
+            if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
+        default:
+            //
+            print("Default switch case")
+        }
+        
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            self.imageView.image = processedImage
+        }
+    }
+    
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
+    }
+    
 }
 
